18b02517f6de9b5ec5d6b1f219ed88b221c3bc0f
albin <albin.abraham@codecraft.co.in>
2023-11-22 11:30:44 +0530

    chore: removed unwanted div, added proper doc for interfaces

--- src/Components/types.ts
+++ src/Components/types.ts
@@ -1,34 +1,48 @@
 /**
- * Props for the NavButton component.
- * @interface
- * @property {string} value - The value of the button.
- * @property {boolean} isActive - Indicates whether the button is active.
- * @property {(value: string) => void} onNavClick - Callback function for button click event.
+ * This interface Props for the NavButton component.
  */
 export interface NavButtonProps {
+  /**
+   * The value of the button.
+   */
   value: string;
+
+  /**
+   * Indicates whether the button is active.
+   */
   isActive: boolean;
+
+  /**
+   * Callback function for button click event.
+   */
   onNavClick: (value: string) => void;
 }
 
 /**
- * Represents the data structure for creating a new Todo.
- * @interface
- * @property {string} title - The title of the Todo.
- * @property {string} description - The description of the Todo.
- * @property {string} dueDate - The due date of the Todo.
+ * This interface is represents the data structure for creating a new Todo.
  */
 export interface CreateTodoData {
+  /**
+   * The title of the Todo.
+   */
   title: string;
+
+  /**
+   * The description of the Todo.
+   */
   description: string;
+
+  /**
+   * The due date of the Todo.
+   */
   dueDate: string;
 }
 
-/**
- * Props for the NavBar component.
- * @interface
- *  setShowModal - Function to set the visibility state of the "Add Todo" modal.
- */
 export interface IProps {
+  /**
+   * This is a function that you can use to control whether the "Add Todo" modal should be visible or hidden.
+   * When we call this function and pass true as an argument,it shows the modal,
+   * and when we pass false, it hides the modal
+   */
   setShowModal: React.Dispatch<React.SetStateAction<boolean>>;
 }
